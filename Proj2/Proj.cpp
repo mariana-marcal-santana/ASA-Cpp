62a7082ff0b8f179471b8f491d3b944996998626
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2023-12-09 20:27:34 +0000

    commit

--- Proj2/Proj.cpp
+++ Proj2/Proj.cpp
@@ -9,31 +9,40 @@
 #include <vector>
 
 int calcMaxSpread(std::vector<std::vector<int>>& matrix) {
-
+    return 0;
 }
 
 int main() {
 
     int n, m;
-    if (!scanf("%d %d", &n, &m));
+    if (!scanf("%d %d", &n, &m)) {}
 
     if (n < 2 || m < 1) {
         printf("0\n");
         return 0;
     }
     
-    std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));
+    std::vector<std::vector<int>> adjList(n, std::vector<int>(n, 0));
+    //std::vector<std::vector<
 
     for (int i = 0; i < m; i++) {
 
         int x, y;
-        if (!scanf("%d %d", &x, &y));
+        if (!scanf("%d %d", &x, &y)) {}
 
-        if (0 < x <= n && 0 < y <= n) {
-            matrix[x - 1][y - 1] = 1;
+        if (0 < x && x <= n && 0 < y && y <= n) {
+            adjList[x - 1][y - 1] = 1;
         }
     }
 
-    printf("%d\n", calcMaxSpread(matrix));
+    for (int i = 0; i < n; i++) {
+        //printf("%d: ", i);
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adjList[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("%d\n", calcMaxSpread(adjList));
     return 0;
 }
