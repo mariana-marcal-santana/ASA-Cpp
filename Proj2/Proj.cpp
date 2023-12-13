ee34260c20e75ad7b7f86ab9b9e9238e11942638
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2023-12-13 14:47:49 +0000

    explained

--- Proj2/Proj.cpp
+++ Proj2/Proj.cpp
@@ -21,98 +21,93 @@ void printStack(std::stack<int> stack) {
 }
 
 class Graph {
-public:
-    int V; // Number of vertices
-    std::vector<std::vector<int>> adjList; // Adjacency list
-    std::vector<std::vector<int>> adjListT; // Transposed adjacency list
+    public:
+        int V; // Number of vertices
+        std::vector<std::vector<int>> adjList; // Adjacency list
+        std::vector<std::vector<int>> adjListT; // Transposed adjacency list
+        Graph(int vertices) : V(vertices), adjList(vertices + 1), adjListT(vertices + 1) {}
+        // Add edge to adjacency list and its transposed to the transposed adjacency list
+        void addEdge(int v, int w) {
+            adjList[v].push_back(w);
+            adjListT[w].push_back(v);
+        }
+        // DFS and fill the stack with vertices in order of finishing time
+        void DFS1(int v, std::vector<bool>& visited, std::stack<int>& descendingEndTimes) {
+            // Initialize stack with starting vertex
+            std::stack<int> dfsStack;
+            dfsStack.push(v);
+            visited[v] = true;
 
-    Graph(int vertices) : V(vertices), adjList(vertices + 1), adjListT(vertices + 1) {}
-
-    // Function to add an edge to the graph
-    void addEdge(int v, int w) {
-        adjList[v].push_back(w);
-        adjListT[w].push_back(v);
-    }
-
-    // Function to perform DFS and fill the stack with vertices in order of finishing time
-    void fillOrder(int v, std::vector<bool>& visited, std::stack<int>& stack) {
-        visited[v] = true;
-        // Use an explicit stack to simulate recursion
-        std::stack<int> dfsStack;
-        dfsStack.push(v);
-
-        while (!dfsStack.empty()) {
-            int currentVertex = dfsStack.top();
-            bool hasNonVisitedNeighbor = false;
-
-            for (int neighbor : adjList[currentVertex]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    hasNonVisitedNeighbor = true;
-                    dfsStack.push(neighbor);
+            while (!dfsStack.empty()) {
+                int currentVertex = dfsStack.top();
+                bool hasNonVisitedNeighbor = false;
+                // Add neighbor vertices to stack if they haven't been visited
+                for (int neighbor : adjList[currentVertex]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        hasNonVisitedNeighbor = true;
+                        dfsStack.push(neighbor);
+                    }
+                }
+                // If all vertex's neighbors were visited, the DFS on the vertex finished
+                if (!hasNonVisitedNeighbor) {
+                    dfsStack.pop();
+                    // Add vertex to stack in order of finishing time
+                    descendingEndTimes.push(currentVertex);
                 }
             }
-            if (!hasNonVisitedNeighbor) {
+        }
+        // Find and set SCCs on a vector with DFS of transposed graph
+        void DFS2(int v, std::vector<bool>& visited, std::vector<int>& scc, int indexSCC) {
+            // Initialize stack with starting vertex and set SCC
+            std::stack<int> dfsStack;
+            dfsStack.push(v);
+            visited[v] = true;
+            scc[v] = indexSCC;
+            
+            while (!dfsStack.empty()) {
+                int currentVertex = dfsStack.top();
                 dfsStack.pop();
-                stack.push(currentVertex);
-            }
-        }
-    }
-
-    // Function to perform DFS starting from a given vertex
-    void DFS(int v, std::vector<bool>& visited, std::vector<int>& scc, int indexSCC) {
-        visited[v] = true;
-        scc[v] = indexSCC;
-        // Use an explicit stack to simulate recursion
-        std::stack<int> dfsStack;
-        dfsStack.push(v);
-
-        while (!dfsStack.empty()) {
-            int currentVertex = dfsStack.top();
-            dfsStack.pop();
-            for (int neighbor : adjListT[currentVertex]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    scc[neighbor] = indexSCC;
-                    dfsStack.push(neighbor);
+                for (int neighbor : adjListT[currentVertex]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        // Set neighbor as part of the same SCC
+                        scc[neighbor] = indexSCC;
+                        dfsStack.push(neighbor);
+                    }
                 }
             }
         }
-    }
 
-    // Function to find Strongly Connected Components using Kosaraju's algorithm
-    void findSCCs() {
-        std::stack<int> stack;
-        std::vector<bool> visited(V + 1, false);
-        std::vector<int> scc(V + 1, 0);
-        int indexSCC = 1;
+        // Function to find SCCs with Kosaraju's algorithm
+        void findSCCs() {
 
-        // Fill vertices in stack according to finishing times
-        for (int i = 1; i < V; i++) {
-            if (!visited[i]) { fillOrder(i, visited, stack); }
-        }
+            std::stack<int> descendingEndTimes;
+            std::vector<bool> visited(V + 1, false);
+            std::vector<int> scc(V + 1, 0);
+            int indexSCC = 1;
 
-        printStack(stack);
-
-        // Reset visited array for the second DFS
-        fill(visited.begin(), visited.end(), false);
-
-        // Process vertices in order defined by the finishing times
-        while (!stack.empty()) {
-            int v = stack.top();
-            stack.pop();
-
-            if (!visited[v]) {
-                DFS(v, visited, scc, indexSCC);
-                indexSCC++;
+            // Set vertices in stack according to finishing times of DFS
+            for (int i = 1; i < V; i++) {
+                if (!visited[i]) { DFS1(i, visited, descendingEndTimes); }
             }
+            printStack(descendingEndTimes);
+            // Reset visited array for DFS2
+            fill(visited.begin(), visited.end(), false);
+            // Process vertices in order defined by the finishing times
+            while (!descendingEndTimes.empty()) {
+                int v = descendingEndTimes.top();
+                descendingEndTimes.pop();
+                // If the vertix hasn't been visited, it belongs to a new SCC
+                if (!visited[v]) {
+                    DFS2(v, visited, scc, indexSCC);
+                    indexSCC++;
+                }
+            }
+            printf("SCC: ");
+            for (int i = 1; i <= V; i++) { printf("%d ", scc[i]); }
+            printf("\n");
         }
-        printf("SCC: ");
-        for (int i = 1; i <= V; i++) {
-            printf("%d ", scc[i]);
-        }
-        printf("\n");
-    }
 };
 
 int main() {
@@ -127,7 +122,6 @@ int main() {
     Graph g(n);
 
     for (int i = 0; i < m; i++) {
-
         int x, y;
         if (!scanf("%d %d", &x, &y)) {}
 
