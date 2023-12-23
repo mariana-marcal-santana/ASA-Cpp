6ec6b2d3edb1bde45844261f7d6180bfb91c4338
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2023-12-23 13:32:07 +0000

    main

--- Proj3/Proj.py
+++ Proj3/Proj.py
@@ -4,3 +4,12 @@ Project made by:
     Mariana Santana (106992)
     João Rodrigues (106221)
 """
+
+def main():
+    # Read input
+    t, p, max = map(int, input().split())
+    n = t
+    for i in range(n):
+        l, c = map(int, input().split())
+    for j in range(p):
+        i, j, k, l = map(int, input().split())
