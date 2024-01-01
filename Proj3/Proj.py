5737683b9bc52fa46adef4d8edb170bbebb96af4
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2024-01-01 14:33:36 +0000

    1300

--- Proj3/Proj.py
+++ Proj3/Proj.py
@@ -15,6 +15,8 @@ def main():
     # Initialize lists for data
     ls = [0] * (t + p + 1)
     cs = [0] * (t + 1)
+    sets = [[i] for i in range(t + 1)]
+    multipliers = [1] * (t + 1) + [3] * p
     # Create list of LP variables
     x = [None] + [LpVariable(f"x{i + 1}", 0, None, LpInteger) for i in range(t + p)]
     # Individual toys
@@ -22,20 +24,24 @@ def main():
         l, c = map(int, input().split())
         ls[n + 1] = l
         cs[n + 1] = c
-        # Individual production constraint
-        prob += x[n + 1] <= c, f"constraint {n + 1}"
     # Toy sets
     for m in range(p):
         i, j, k, l = map(int, input().split())
         ls[m + t + 1] = l
+        sets[i].append(m + t + 1)
+        sets[j].append(m + t + 1)
+        sets[k].append(m + t + 1)
+    #print(sets)
     # Max production constraint
-    prob += lpSum([x[i] for i in range(1, len(x))]) <= max, "constraint max"
+    prob += lpSum([x[i] * multipliers[i] for i in range(1, len(x))]) <= max, "constraint max"
+    # Toy sets constraint
+    for i in range(1, len(sets)):
+        prob += lpSum([x[j] for j in sets[i]]) <= cs[i], f"constraint set {i}"
     # Set objective function
-    prob += lpSum([x[i] for i in range(1, len(x))]), "objective function"
-    prob += lpSum([x[i] * l[i] for i in range(1, len(x))]), "objective function"
+    prob += lpSum([x[i] * ls[i] for i in range(1, len(x))]), "objective function"
 
-    print(ls)
-    print(cs)
+    #print(ls)
+    #print(cs)
 
     prob.solve(GLPK(msg = 0))
 
@@ -43,7 +49,7 @@ def main():
         print("Infeasible")
     else:
         print(value(prob.objective))
-        for v in prob.variables():
-            print(v.name, "=", v.varValue)
+        #for v in prob.variables():
+            #print(v.name, "=", v.varValue)
 
 main()
