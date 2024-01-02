4f271b480b6d6006f00b9ef446bbc7fc7f6022a4
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2024-01-02 16:57:38 +0000

    1700 commented

--- Proj3/Proj.py
+++ Proj3/Proj.py
@@ -13,17 +13,15 @@ def main():
     # Read input
     t, p, max = map(int, input().split())
     # Initialize lists for data
-    ls = [0]
-    cs = [0]
+    ls, cs, x = [0], [0], [None]
     sets = [[i] for i in range(t + 1)]
     multipliers = [1] * (t + 1) + [3] * p
-    # Create list of LP variables
-    x = [None] + [LpVariable(f"x{i + 1}", 0, None, LpInteger) for i in range(t + p)]
     # Individual toys
     for n in range(t):
         l, c = map(int, input().split())
         ls.append(l)
         cs.append(c)
+        x += [LpVariable(f"x{n + 1}", 0, c, LpInteger)]
     # Toy sets
     for m in range(p):
         i, j, k, l = map(int, input().split())
@@ -31,9 +29,10 @@ def main():
         sets[i].append(m + t + 1)
         sets[j].append(m + t + 1)
         sets[k].append(m + t + 1)
+        x += [LpVariable(f"x{m + t + 1}", 0, min(cs[i], cs[j], cs[k]), LpInteger)]
     # Max production constraint
     prob += lpSum([x[i] * multipliers[i] for i in range(1, len(x))]) <= max, "constraint max"
-    # Toy sets constraint
+    # Toy sets constraints
     for i in range(1, len(sets)):
         prob += lpSum([x[j] for j in sets[i]]) <= cs[i], f"constraint set {i}"
     # Set objective function
